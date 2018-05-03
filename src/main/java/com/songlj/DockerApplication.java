c95b3aea03dcbdc7d0de0ee5558def904eccbe2d
songlj <1107834334@qq.com>
2018-05-04 00:58:24 +0800

    Initial commit

---
+++ src/main/java/com/songlj/DockerApplication.java
@@ -0,0 +1,18 @@
+package com.songlj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker!!看到这句话没有？";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+}
